13088ff6faea5036d4437feee33107fe78cc6422
Oliver Frolovs <oliver.frolovs@bristol.ac.uk>
2019-12-10 07:01:51 +0000

    Update rotor_interface.m
    Clean up status handling

--- rigs/rotor/rotor_interface.m
+++ rigs/rotor/rotor_interface.m
@@ -24,7 +24,7 @@ classdef rotor_interface < rtc_interface
             obj.par.bbb_reboot = 1;
         end
         
-        function reset_status_flags(obj)
+        function clear_status(obj)
             obj.par.reset_status_flags = 1;
         end
         
@@ -36,37 +36,37 @@ classdef rotor_interface < rtc_interface
             v = bitand(obj.par.status_flags, obj.RIG_STATUS_SPEED_SAFETY_LIMIT_REACHED);
         end
         
-        function print_status(obj)
-            status = obj.par.status_flags;
+        % TODO clear_status (to replace reset_status and provide granularity)
+        
+        function status(obj)
+            flags = obj.par.status_flags;
             
-            if status == obj.RIG_STATUS_OK
+            if flags == obj.RIG_STATUS_OK
                 disp("  * Rig status OK")
             else
-                disp("  * Rig error flags are set: ");
-                
-                if bitand(status, obj.RIG_STATUS_UNKNOWN_ERROR)
-                    warning("General (unknown) error");
+                if bitand(flags, obj.RIG_STATUS_UNKNOWN_ERROR)
+                    warning("Rig error: unknown error");
                 end
 
-                if bitand(status, obj.RIG_STATUS_SETUP_NOT_COMPLETED)
-                    warning("Rig set up after reboot has not been finished");
+                if bitand(flags, obj.RIG_STATUS_SETUP_NOT_COMPLETED)
+                    warning("Rig error: rig set up after reboot has not been finished");
                 end
 
-                if bitand(status, obj.RIG_STATUS_PID_NUMERIC_ERROR)
-                    warning("PID controller numerical error");
+                if bitand(flags, obj.RIG_STATUS_PID_NUMERIC_ERROR)
+                    warning("Rig error: PID controller numerical error");
                 end
                 
-                if bitand(status, obj.RIG_STATUS_SPEED_SAFETY_LIMIT_REACHED)
-                    warning("Safety stop (%.1f rpm) was activated at %.1f rpm", ...
+                if bitand(flags, obj.RIG_STATUS_SPEED_SAFETY_LIMIT_REACHED)
+                    warning("Rig error: safety stop (%.1f rpm) was activated at %.1f rpm", ...
                         obj.par.speed_safety_limit_rpm, obj.par.safety_triggered_speed_rpm);
                 end
                 
-                if bitand(status, obj.RIG_STATUS_MOTOR_VOLTAGE_CLIP_AT_MIN)
+                if bitand(flags, obj.RIG_STATUS_MOTOR_VOLTAGE_CLIP_AT_MIN)
                     warning("Motor voltage was clipped at minimum value of %.1f", ...
                         obj.par.motor_min_voltage);
                 end
                 
-                if bitand(status, obj.RIG_STATUS_MOTOR_VOLTAGE_CLIP_AT_MAX)
+                if bitand(flags, obj.RIG_STATUS_MOTOR_VOLTAGE_CLIP_AT_MAX)
                     warning("Motor voltage was clipped at maximum value of %.1f", ...
                         obj.par.motor_max_voltage);
                 end
